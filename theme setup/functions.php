004d7642907d0fe9c81444c8db38fd3ad2791816
MD: Tuhin Uz-Zaman (Hamim) <75517293+hamim6850@users.noreply.github.com>
2022-03-13 12:03:46 +0600

    Update functions.php

--- theme setup/functions.php
+++ theme setup/functions.php
@@ -1,1 +1,47 @@
+<?php
 
+function your_theme_name_setup() {
+	
+	load_theme_textdomain( 'your_theme_name', get_template_directory() . '/languages' );
+
+	add_theme_support( 'automatic-feed-links' );
+
+	
+	add_theme_support( 'title-tag' );
+
+	
+	add_theme_support( 'post-thumbnails' );
+
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Primary', 'your_theme_name' ),
+	) );
+
+
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'comment-list',
+		'caption',
+	) );
+
+	add_theme_support( 'custom-background', apply_filters( 'your_theme_name_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+
+}
+
+add_action( 'after_setup_theme', 'your_theme_name_setup' );
+
+function your_theme_name_add_editor_styles() {
+	add_editor_style( 'custom-editor-style.css' );
+}
+add_action( 'admin_init', 'your_theme_name_add_editor_styles' );
+
+function your_theme_name_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'your_theme_name_content_width', 1170 );
+}
+add_action( 'after_setup_theme', 'your_theme_name_content_width', 0 );
