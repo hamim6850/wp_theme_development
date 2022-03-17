f9265f39d905bf66b4a3f328ae9c0799372eb963
MD: Tuhin Uz-Zaman (Hamim) <75517293+hamim6850@users.noreply.github.com>
2022-03-18 00:39:00 +0600

    Update hide-admin-toolbar.php

--- theme setup/hide-admin-toolbar.php
+++ theme setup/hide-admin-toolbar.php
@@ -1,14 +1,10 @@
 <?php
 
 //hide admin bar
-define('AMM_HAT_VERSION', '1.0');
-define('AMM_HAT_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('AMM_HAT_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 add_filter('show_admin_bar', '__return_false');
-add_action('admin_print_scripts-profile.php', 'amm_hat_add_css_style');
 
-function amm_hat_add_css_style() {
+function hide_admin_bar() {
     ?>
     <style type="text/css">
         .show-admin-bar {display: none !important;}
@@ -16,4 +12,5 @@ function amm_hat_add_css_style() {
     <?php
 
 }
+add_action('admin_print_scripts-profile.php', 'hide_admin_bar');
 //end
